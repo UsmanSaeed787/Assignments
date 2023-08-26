76a060ee51edae717836af62532835025734f05e
Usman_Saeed <usmanalisaeed787@gmail.com>
2023-08-26 20:20:36 +0500

    Learning TypeScript

---
+++ index1.js
@@ -0,0 +1,32 @@
+"use strict";
+/* - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."*/
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT.");
+/* - Store your name in a variable and print it.*/
+var nam = "Usman Saeed";
+console.log("My Name is", nam);
+/* - Store 10 numbers in different variables.*/
+var num1 = 100;
+var num2 = 90;
+var num3 = 80;
+var num4 = 70;
+var num5 = 60;
+var num6 = 50;
+var num7 = 40;
+var num8 = 30;
+var num9 = 20;
+var num10 = 10;
+/*   - Add all of them and print the SUM.*/
+var sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("Total No. of Sum is =", sum);
+//   - Print the Difference (subtraction).
+var diff = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("Total No. of Diff is =", diff);
+//   - Print the result after multiplying all.
+var multi = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("Total value of Multiply is =", multi);
+// - Take two numbers and print the division result.
+var div = num1 / num10;
+console.log("The value of Division is =", div);
+// - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+var allopr = num1 + num2 - (num3 * num4) / num5 + num6 - (num7 * num8) / num9 + num10;
+console.log("Perform all operations ", allopr);
---
+++ index1.ts
@@ -0,0 +1,48 @@
+/* - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."*/
+console.log("Hey Abu Hurairah, I have started learning TYPESCRIPT.");
+
+/* - Store your name in a variable and print it.*/
+var nam: string = "Usman Saeed";
+console.log("My Name is", nam);
+
+/* - Store 10 numbers in different variables.*/
+
+var num1: number = 100;
+var num2: number = 90;
+var num3: number = 80;
+var num4: number = 70;
+var num5: number = 60;
+var num6: number = 50;
+var num7: number = 40;
+var num8: number = 30;
+var num9: number = 20;
+var num10: number = 10;
+
+/*   - Add all of them and print the SUM.*/
+
+var sum: number =
+    num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("Total No. of Sum is =", sum);
+
+//   - Print the Difference (subtraction).
+
+var diff: number =
+    num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("Total No. of Diff is =", diff);
+
+//   - Print the result after multiplying all.
+
+var multi: number =
+    num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("Total value of Multiply is =", multi);
+
+// - Take two numbers and print the division result.
+
+var div: number = num1 / num10;
+console.log("The value of Division is =", div);
+
+// - Now, perform all four operations with the given numbers, print the results, and observe the order in which the operations take place.
+
+var allopr: number =
+    num1 + num2 - (num3 * num4) / num5 + num6 - (num7 * num8) / num9 + num10;
+console.log("Perform Multiple operations ", allopr);
